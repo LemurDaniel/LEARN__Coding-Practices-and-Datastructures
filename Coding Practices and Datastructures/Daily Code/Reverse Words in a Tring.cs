4e712ef8524f3c0868d1eaa2ef8383e748791fa3
Daniel Landau <Landau.Daniel.1998@gmail.com>
2019-09-07 18:46:17 +0200

    DailyCode Reverse Words in a String

---
+++ Coding Practices and Datastructures/Daily Code/Reverse Words in a Tring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Practices_and_Datastructures.Daily_Code
+{
+    class Reverse_Words_in_a_String : Testable
+    {
+        public class InOut : InOutBase<string, string>
+        {
+            public InOut (string s, string s2) : base(s, s2, true)
+            {
+                AddSolver(ReverseWords);
+            }
+        }
+
+        public Reverse_Words_in_a_String()
+        {
+            testcases.Add(new InOut("The cat in the hat", "ehT tac ni eht tah"));
+        }
+
+
+        //SOL
+        public static void ReverseWords(string s, InOut.Ergebnis erg)
+        {
+            string temp = "";
+            StringBuilder reverse = new StringBuilder();
+            foreach(char c in s){
+                if (c != ' ') temp = c + temp;
+                else
+                {
+                    reverse.Append(temp + c);
+                    temp = "";
+                }
+            }
+            reverse.Append(temp);
+            erg.Setze(reverse.ToString());
+        }
+    }
+}
--- Coding Practices and Datastructures/Program.cs
+++ Coding Practices and Datastructures/Program.cs
@@ -160,7 +160,8 @@ namespace GoF_Coding_Interview_Algos
             //  aufgabe = new Minimal_Removals_for_Valid_Parenthesises();
             //  aufgabe = new Words_that_are_Anagrams();
             //  aufgabe = new Running_Median();
-            aufgabe = new Room_Scheduling();
+            //  aufgabe = new Room_Scheduling();
+            aufgabe = new Reverse_Words_in_a_String();
 
             while (true) { aufgabe.Test(); }
         }
