f6c6c192f92f004d37d37884602aacee68259741
LemurDaniel <landau.daniel.1998@gmail.com>
2021-04-18 03:10:58 +0200

    ...

--- nodejs/codingQuestions/arrays/sort_colors.js
+++ nodejs/codingQuestions/arrays/sort_colors.js
@@ -55,7 +55,9 @@ Inout.solve();
     
     The value at ptr_start will always be a one. It can't be a two, because the loop
     would have already checked the value at that index and moved it to the end. Can't
-    be a zero either since it would already been moved too at this point.
+    be a zero either since it would have already been moved too at this point. 
+    The value at ptr_start can only be a one when the current index i in the loop
+    euqals ptr_start which has no effect at all.
 
 */
 
