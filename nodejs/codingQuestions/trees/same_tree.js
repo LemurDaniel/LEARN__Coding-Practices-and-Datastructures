ca9d354fdda289ef3b6b851294a468bfd971dbe4
LemurDaniel <landau.daniel.1998@gmail.com>
2021-04-24 11:51:10 +0200

    Coding Problems is same tree iterative

--- nodejs/codingQuestions/trees/same_tree.js
+++ nodejs/codingQuestions/trees/same_tree.js
@@ -33,7 +33,7 @@ Inout.push( {
     tree_1: '&BT %1,4,3,$5,$2,5,$5,-1',
     tree_2: '&BT %1,4,3,$5,$2,5,$4,-1' }, false );
 
-Inout.solvers = [same_tree_recursive];
+Inout.solvers = [same_tree_recursive, same_tree_iterative];
 Inout.solve();
 
 
@@ -58,4 +58,35 @@ function same_tree_recursive (node_1, node_2) {
 
     return same_tree_recursive(node_1.left, node_2.left) &&
         same_tree_recursive(node_1.right, node_2.right);
+}
+
+function same_tree_iterative (tree_1, tree_2) {
+    
+    let node_1 = tree_1.root;
+    let node_2 = tree_2.root;
+
+    const stack_1 = [node_1];
+    const stack_2 = [node_2];
+
+    while(stack_1.length > 0) {
+
+        if( !node_1 && !node_2 ) {
+            node_1 = stack_1.pop().right;
+            node_2 = stack_2.pop().right;
+            continue;
+        }
+
+        // return false when only one node is null
+        if( !node_1 || !node_2 ) return false;
+
+        if(node_1.val != node_2.val) return false;
+
+        stack_1.push(node_1);
+        stack_2.push(node_2);
+
+        node_1 = node_1.left;
+        node_2 = node_2.left;
+    }
+
+    return stack_1.length == stack_2.length;
 }
