afc83ce565ad991bbb88e86567b496bcc058b568
LemurDaniel <landau.daniel.1998@gmail.com>
2021-04-28 18:49:30 +0200

    Fixed for failing testcase 99999 + 1

--- nodejs/codingQuestions/linked_lists/add_two_numbers_I.js
+++ nodejs/codingQuestions/linked_lists/add_two_numbers_I.js
@@ -9,6 +9,9 @@ const Helper = require('../../Helper');
 
     You may assume the two numbers do no contain any leading zero, except the number 0  itself.
 
+    Most significant digit is on the right therefore all numers must be read from the right to left
+        ==> 2 => 4 => 3  is number: 342
+
     Example:
         Input:  (2 => 4 => 3) + (5 => 6 => 4)
         Output: 7 => 0 => 8
@@ -16,13 +19,14 @@ const Helper = require('../../Helper');
 
 Inout.input_string_converter = arg => '(' + arg.list.toString() + ') + (' + arg.list_2.toString() + ')';
 
+Inout.push( { list: '9999999', list_2: '&LL 1' } , '&LL 00000001' );
 Inout.push( { list: '243', list_2: '&LL 564' } , '&LL 708' );
 Inout.push( { list: '243', list_2: '&LL 566' } , '&LL 7001' );
 Inout.push( { list: '243', list_2: '&LL 5669' } , '&LL 70001' );
 Inout.push( { list: '24333', list_2: '&LL 564' } , '&LL 70833' );
 Inout.push( { list: '243', list_2: '&LL 56444' } , '&LL 70844' );
 
-Inout.solvers = [add_two_numbers];
+Inout.solvers = [add_two_numbers, add_two_numbers_in_new_list];
 Inout.solve();
 
 /*
@@ -39,18 +43,17 @@ function add_two_numbers (num, num2)  {
     let node_2 = num2.head;
 
     let carry = 0;
-    while(node_1 && node_2) {
-        const sum   = node_1.val + node_2.val + carry;
+    while(node_1) {
+        const sum   = node_1.val + (node_2 ? node_2.val : 0) + carry;
         node_1.val  = sum % 10;
-        carry = Math.floor(sum / 10)
+        carry       = Math.floor(sum / 10)
 
-        if(carry && !node_1.next) {
-            num.Append(1);
-            carry = 0;
-        }
+        // handles carries beyond nums most significant digit
+        // Example: 999 + 1 ==> 0001 (Additional node with carry is needed after 999 most significant digit) 
+        if(carry && !node_1.next) num.Append(0);
 
         node_1 = node_1.next;
-        node_2 = node_2.next;
+        node_2 = node_2 ? node_2.next : null;
     }
 
     // handle additional nodes if num2 is bigger than num 
@@ -60,4 +63,32 @@ function add_two_numbers (num, num2)  {
     }
 
     return num;
+}
+
+
+function add_two_numbers_in_new_list (num, num2)  {
+
+    let node_1 = num.head;
+    let node_2 = num2.head;
+    const summed = new LinkedList();
+
+    let carry = 0;
+    while(node_1) {
+        const sum   = node_1.val + (node_2 ? node_2.val : 0) + carry;
+        carry       = Math.floor(sum / 10)
+        summed.Append(sum % 10);
+
+        if(carry && !node_1.next) num.Append(0);
+
+        node_1 = node_1.next;
+        node_2 = node_2 ? node_2.next : null;
+    }
+
+    // handle additional nodes if num2 is bigger than num 
+    while(node_2) {
+        summed.Append(node_2.val);
+        node_2 = node_2.next;
+    }
+
+    return summed;
 }
