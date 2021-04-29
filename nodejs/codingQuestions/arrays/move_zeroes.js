3fc24cce042e82a6e669c2db77cf8bc65cf8434b
LemurDaniel <landau.daniel.1998@gmail.com>
2021-04-29 21:20:05 +0200

    ...

--- nodejs/codingQuestions/arrays/move_zeroes.js
+++ nodejs/codingQuestions/arrays/move_zeroes.js
@@ -16,6 +16,7 @@ const Helper = require('../../Helper');
 
 Inout.push('&AR 0,1,0,3,12', '&AR 1,3,12,0,0')
 Inout.push('&AR 0,1,0,3,12,34,0,8,2,0,0,0,90,0,44,42,0,39', '&AR 1,3,12,34,8,2,90,44,42,39,0,0,0,0,0,0,0,0')
+Inout.push('&AR 1,3,12,34,8,2,0,0,0,90,0,44,42,0,0,0,0,39', '&AR 1,3,12,34,8,2,90,44,42,39,0,0,0,0,0,0,0,0')
 
 Inout.solvers = [move_zeroes];
 Inout.solve();
@@ -29,7 +30,7 @@ Inout.solve();
 
 /*
     space complexity: O(1)  - constant
-    time  complexity: O(n)  - linear (one pass)
+    time  complexity: O(n)  - linear
 */
 
 function move_zeroes (array)  {
@@ -39,15 +40,14 @@ function move_zeroes (array)  {
 
    for(let i=0; i<array.length; i++) {
         
-        // move all non zero elements to start of array
+        // ptr_start always points to the index after the last moved non-zero element at the start of the array
+        // via this all non-zero elements are gradually moved to the start of the array
         if(array[i] != 0) array[ptr_start++] = array[i];
-        // count number of 0's in array
         else count_zeros++;
-
-        // array length equals the sum of the number of zero and non-zero values
-        // array.length = count of 0's + count of non-0's
-        // ==> index of first 0 is at => array.length - count_zeroes
-        // ==> if current index 'i' is at or beyond this index, start replacing all following values with 0's
-        if( i >= array.length - count_zeros ) array[i] = 0;
    }
+
+   // replace all index from the end of the array with zeroes,
+   // according to the number of counted zeroes
+   while(count_zeros > 0)  array[ array.length - count_zeros-- ] = 0;
+
 }
