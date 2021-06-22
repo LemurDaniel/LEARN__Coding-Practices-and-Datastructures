0300937bed4ccbe98110c99581cae4c909cfa682
LemurDaniel <landau.daniel.1998@gmail.com>
2021-06-22 10:27:20 +0200

    bla

--- nodejs/dailyCodingProblem/232-[Easy].js
+++ nodejs/dailyCodingProblem/232-[Easy].js
@@ -5,10 +5,19 @@ const Helper = require('../Helper');
 
     Good morning! Here's your coding interview problem for today.
 
-    This problem was asked by Twitter.
+    This problem was asked by Google.
 
-    Given a list of numbers, create an algorithm that arranges them in order to form the largest possible integer. 
-    For example, given [10, 7, 76, 415], you should return 77641510.
+    Implement a PrefixMapSum class with the following methods:
+
+    insert(key: str, value: int): Set a given key's value in the map. If the key already exists, overwrite the value.
+    sum(prefix: str): Return the sum of all values of keys that begin with a given prefix.
+    For example, you should be able to run the following code:
+
+    mapsum.insert("columnar", 3)
+    assert mapsum.sum("col") == 3
+
+    mapsum.insert("column", 2)
+    assert mapsum.sum("col") == 5
 
 */
 
