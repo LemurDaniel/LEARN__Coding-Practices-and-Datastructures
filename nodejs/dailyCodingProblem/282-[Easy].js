4eec30334aac58288aa3e3254a95c9c7c6583b13
LemurDaniel <landau.daniel.1998@gmail.com>
2021-08-10 12:12:26 +0200

    dailyCodingProblem phytagorean triplet

--- nodejs/dailyCodingProblem/282-[Easy].js
+++ nodejs/dailyCodingProblem/282-[Easy].js
@@ -14,7 +14,7 @@ const Inout = new (require("../Inout"))("Daily Coding Problem --- Phytagorean tr
 Inout.push('&AR 3,1,4,6,5', '&AR 3,4,5');
 Inout.push('&AR 10,4,6,12,5', false);
 
-Inout.solvers = [phytagoreanTriplet_bruteForce]
+Inout.solvers = [phytagoreanTriplet_bruteForce, phytagoreanTriplet_hashing, phytagoreanTriplet_hashing2]
 Inout.solve();
 
 /*
@@ -24,18 +24,80 @@ Inout.solve();
     ###########################################################################################
 */
 
+// Time complexity: (n^3)  -  Cubic
+// Space complxity: 1 - constant
+
 function phytagoreanTriplet_bruteForce(nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+
+            // First too loops multiply all numbers with eacht other.
+            const [a, b] = [nums[i], nums[j]];
+            const cpow2 = a * a + b * b;
+
+            // Third loop checks whether resultung c^2 is in the array.
+            for (let k = 0; k < nums.length; k++) {
+
+                const c = nums[k];
+                if (c * c === cpow2)
+                    return [a, b, c];
+            }
+        }
+    }
+
+    return false;
+}
+
+// Time complexity: (n + n^2)  -  Quadratic
+// Space complxity: n - linear
+
+function phytagoreanTriplet_hashing(nums) {
 
     const dict = {};
     for (const num of nums)
         dict[num * num] = num;
 
 
-    for (const a of nums) {
-        for (const b of nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+
+            // Works like upper method but replaces third loop with a dictionary for looking up the value.
+            if (i === j) continue;
+
+            const [a, b] = [nums[i], nums[j]];
             const cpow2 = a * a + b * b;
             if (cpow2 in dict)
                 return [a, b, dict[cpow2]]
+        }
+    }
+
+    return false;
+}
+
+// Time complexity: (n + n^2)  -  Quadratic
+// Space complxity: n - linear
+
+function phytagoreanTriplet_hashing2(nums) {
+
+    // Simulate using only a hashset like in java for example.
+    const dict = {};
+    for (const num of nums)
+        dict[num] = null;
+
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+
+            if (i === j) continue;
+
+            const [a, b] = [nums[i], nums[j]];
+            const cpow2 = a * a + b * b;
+            const c = Math.round(Math.sqrt(cpow2));
+            if( c*c !== cpow2) continue;
+
+            if (c in dict)
+                return [a, b, c]
         }
     }
 
