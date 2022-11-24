9061af6b99c4d8d090fe789a616657e84cc84f14
LemurDaniel <landau.daniel.1998@gmail.com>
2022-11-24 03:00:19 +0100

    Changes

--- advent_of_code/blueprint.js
+++ advent_of_code/blueprint.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('input/${{INPUT_NORMAL}}', 'utf-8').split('\r\n');
-// const input = fs.readFileSync('input/${{INPUT_TEST}}', 'utf-8').split('\r\n');
+const input = fs.readFileSync('input/${{INPUT_TEST}}', 'utf-8').split('\r\n');
