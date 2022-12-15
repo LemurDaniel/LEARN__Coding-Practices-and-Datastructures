84254e5ea82050d279b14f7f3d00919dfc3296cc
LemurDaniel <landau.daniel.1998@gmail.com>
2022-12-15 23:36:05 +0100

    AOC Day 15

---
+++ advent_of_code/2022-nodejs/day15-part1.js
@@ -0,0 +1,174 @@
+const { Datastructures, Utils } = require('../_lib/lib.js')
+const process = require('process')
+const fs = require('fs');
+
+// NOTE:
+//  node .\day15-part1.js [INPUT|TEST]
+//
+//  Enter:
+//    'node .\day15-part1.js INPUT'    to process Todays input from 'day15-input.txt'.
+//
+//  Enter:
+//    'node .\day15-part1.js TEST'     to process Todays Testinput from 'day15-input-test.txt'.
+//    'node .\day15-part1.js'          to process Todays Testinput from 'day15-input-test.txt'.
+
+const argument = (process.argv[2] ?? 'TEST').toUpperCase()
+switch (argument.toUpperCase()) {
+  case 'TEST':
+    fileContent = fs.readFileSync('input/day15-input-test.txt', 'utf-8'); break
+  case 'INPUT':
+    fileContent = fs.readFileSync('input/day15-input.txt', 'utf-8'); break
+
+  default:
+    throw 'Argument not Valid'
+}
+
+// Prepare Input
+const input = fileContent.split('\r\n')
+
+// Get Needed Classes
+const Vector = Datastructures.Vector
+
+/*
+    ###########################################################################################
+    ###########################################################################################
+    ####################               Solve AOC below                  #######################
+    ###########################################################################################
+*/
+
+
+class Beacon extends Vector { }
+class Sensor extends Vector { }
+Vector.prototype.toString = function toString() {
+  return `${this.constructor.name}(${this.x.toLocaleString()}, ${this.y.toLocaleString()})`
+}
+
+const SENSORS = {}
+const BEACONS = {}
+const BOUNDS = {
+  max: Vector.NULL,
+  min: Vector.NULL
+
+}
+
+
+///////////////////////////////////////////////////////////////
+
+// Process Input
+
+const test = input.map(
+  line => line.split(':').map(
+    segment => segment.match(/-{0,1}\d+/g)
+  )
+).forEach(line => {
+  const sensor = new Sensor(...line[0])
+  const beacon = new Beacon(...line[1])
+  BOUNDS.max.x = Math.max(sensor.y, beacon.x, BOUNDS.max.x)
+  BOUNDS.max.y = Math.max(sensor.y, beacon.y, BOUNDS.max.y)
+  BOUNDS.min.x = Math.min(sensor.y, beacon.x, BOUNDS.min.x)
+  BOUNDS.min.y = Math.min(sensor.y, beacon.y, BOUNDS.min.y)
+  SENSORS[sensor] = sensor
+  BEACONS[sensor] = beacon
+})
+
+
+
+///////////////////////////////////////////////////////////////
+
+
+// Specify Target-Y-Axis, then generate all Intersection-Ranges for all Sensors on it.
+const targetY = argument.includes('TEST') ? 10 : 2_000_000
+let intersectionRanges = []
+
+for (let sensor of Object.values(SENSORS)) {
+
+  const beacon = BEACONS[sensor]
+  const signalStrength = sensor.manhattenDist(beacon)
+
+  // First disregard all sensors with Signal Strength Out of Reach for specified Y-Axis
+  if (Math.abs(targetY - sensor.y) > signalStrength)
+    continue
+
+  // Calculate the Range of Intersection on the Y-Axis that is searched for.
+
+  //          Left <== SignalReach ==> Right
+  //      . . . # # # # # # S # # # # # # . . .
+  //      . . . . # # # # # # # # # # # . . . .
+  //      . . . . . # # # # # # # # # . . . . .
+  //      . . . . . . # # # # # # # . . . . . . <== TargetY
+  //                  ^           ^
+  //          Intersection Range on Specified Y
+
+  // On the Y-Axis of the Sensor the covered X-Positions Reach from 
+  // 'sensorPosX - SignalStrengt' on the Left and 'sensorPosX + signalStrength' on the Right
+  const signalReachXLeft = Vector.sub(sensor, signalStrength)
+  const signalReachXRight = Vector.add(sensor, signalStrength)
+
+  // The signalStrengthRange on the Y-Axis decreases by one, the Further the distance between the 'sensorY' and 'targetY' by 1
+  const diffY = Math.abs(targetY - sensor.y)
+  // Subtract the Difference on the Y-Axis from the X-Axis of the signalReachRanges on both sides
+  signalReachXLeft.add(diffY)
+  signalReachXRight.sub(diffY)
+  signalReachXLeft.y = targetY
+  signalReachXRight.y = targetY
+
+  // The Manhattendistance of both Vectors to the Sensor is now equal to the signalStrength and both of them mark 
+  // the Start and End of the Range of SignalIntersection on the targetY.
+
+  // Add to array for further processing, since signal ranges could potentially overlap
+  // (-4, 10) => (14, 10) with (-2, 10) => (8, 10)
+  intersectionRanges.push([signalReachXLeft, signalReachXRight])
+}
+
+///////////////////////////////////////////////////////////////
+
+
+function mergeRanges(intersectionRanges) {
+
+  const intialRangesCount = intersectionRanges.length
+  intersectionRanges.sort((a, b) => b[0].x - a[0].x)
+  const finalIntersections = [intersectionRanges.pop()]
+
+  while (intersectionRanges.length > 0) {
+
+    const [rangeStartCurr, rangeEndCurr] = finalIntersections[finalIntersections.length - 1]
+    const [rangeStartNext, rangeEndNext] = intersectionRanges.pop()
+
+    // Disregard if the Range is already containted in the existing range
+    if (rangeStartNext.x > rangeStartCurr.x && rangeEndNext.x < rangeEndCurr.x)
+      continue
+
+    const expandStart = rangeStartNext.x < rangeStartCurr.x && rangeEndNext.x >= rangeStartCurr.x
+    const expandEnd = rangeEndNext.x > rangeEndCurr.x && rangeStartNext.x <= rangeEndCurr.x
+    if (expandStart)
+      rangeStartCurr.x = rangeStartNext.x
+
+    if (expandEnd)
+      rangeEndCurr.x = rangeEndNext.x
+
+    if (!expandStart && !expandEnd)
+      finalIntersections.push([rangeStartNext, rangeEndNext])
+
+  }
+
+  // TODO Improve, since now on the merge ranges, the method has to be called again, to make sure those can't be merged further
+  if (intialRangesCount != finalIntersections.length)
+    return mergeRanges(finalIntersections)
+  else
+    return finalIntersections
+}
+
+///////////////////////////////////////////////////////////////
+
+
+// Merge any Overlapping Ranges
+intersectionRanges = mergeRanges(intersectionRanges)
+
+// Get final count of covered positions by processed ranges.
+const coveredPositions = intersectionRanges.reduce((covered, range) => covered + Math.abs(range[0].x - range[1].x), 0)
+
+console.group()
+console.log('\nCalculated and merged intersection ranges: ')
+console.log(Utils.Print.fromArray(intersectionRanges, 0, value => `${value[0].toString()} ==> ${value[1].toString()}`))
+console.log(`\nOn the Y-Axis at ${targetY.toLocaleString()} there are a total of ${coveredPositions.toLocaleString()} that can't have a beacon.\n`)
+console.groupEnd()
---
+++ advent_of_code/2022-nodejs/day15-part2.js
@@ -0,0 +1,193 @@
+const { Datastructures, Utils } = require('../_lib/lib.js')
+const process = require('process')
+const fs = require('fs');
+
+// NOTE:
+//  node .\day15-part2.js [INPUT|TEST]
+//
+//  Enter:
+//    'node .\day15-part2.js INPUT'    to process Todays input from 'day15-input.txt'.
+//
+//  Enter:
+//    'node .\day15-part2.js TEST'     to process Todays Testinput from 'day15-input-test.txt'.
+//    'node .\day15-part2.js'          to process Todays Testinput from 'day15-input-test.txt'.
+
+const argument = (process.argv[2] ?? 'TEST').toUpperCase()
+switch (argument.toUpperCase()) {
+  case 'TEST':
+    fileContent = fs.readFileSync('input/day15-input-test.txt', 'utf-8'); break
+  case 'INPUT':
+    fileContent = fs.readFileSync('input/day15-input.txt', 'utf-8'); break
+
+  default:
+    throw 'Argument not Valid'
+}
+
+// Prepare Input
+const input = fileContent.split('\r\n')
+
+// Get Needed Classes
+const Vector = Datastructures.Vector
+
+/*
+    ###########################################################################################
+    ###########################################################################################
+    ####################               Solve AOC below                  #######################
+    ###########################################################################################
+*/
+
+class Beacon extends Vector { }
+class Sensor extends Vector { }
+Vector.prototype.toString = function toString() {
+  return `${this.constructor.name}(${this.x.toLocaleString()}, ${this.y.toLocaleString()})`
+}
+
+const SENSORS = {}
+const BEACONS = {}
+const BOUNDS = {
+  max: Vector.NULL,
+  min: Vector.NULL
+
+}
+
+
+///////////////////////////////////////////////////////////////
+
+// Process Input
+input.map(
+  line => line.split(':').map(
+    segment => segment.match(/-{0,1}\d+/g)
+  )
+).forEach(line => {
+  const sensor = new Sensor(...line[0])
+  const beacon = new Beacon(...line[1])
+  BOUNDS.max.x = Math.max(sensor.y, beacon.x, BOUNDS.max.x)
+  BOUNDS.max.y = Math.max(sensor.y, beacon.y, BOUNDS.max.y)
+  BOUNDS.min.x = Math.min(sensor.y, beacon.x, BOUNDS.min.x)
+  BOUNDS.min.y = Math.min(sensor.y, beacon.y, BOUNDS.min.y)
+  SENSORS[sensor] = sensor
+  BEACONS[sensor] = beacon
+})
+
+
+///////////////////////////////////////////////////////////////
+
+const cached = {}
+function GetIntersectionRangesFor(target, Axis) {
+
+  Axis = Axis.toUpperCase()
+  if (Axis != 'Y' && Axis != 'X') throw 'Error'
+
+  if (cached[[target, Axis]])
+    return cached[[target, Axis]]
+
+  let intersectionRanges = []
+
+  for (const sensor of Object.values(SENSORS)) {
+
+    const beacon = BEACONS[sensor]
+    const signalStrength = sensor.manhattenDist(beacon)
+
+    // First disregard all sensors with Signal Strength Out of Reach for specified Y-Axis
+    if (Axis == 'Y' && Math.abs(target - sensor.y) > signalStrength)
+      continue
+
+    if (Axis == 'X' && Math.abs(target - sensor.x) > signalStrength)
+      continue
+
+
+    if (Axis == 'Y') {
+      const diffY = Math.abs(target - sensor.y)
+      const signalReachLeft = [sensor.x - signalStrength + diffY, target]
+      const signalReachRight = [sensor.x + signalStrength - diffY, target]
+
+      intersectionRanges.push([signalReachLeft, signalReachRight])
+    } else if (Axis == 'X') {
+      const diffX = Math.abs(target - sensor.x)
+      const signalReachDown = [target, sensor.y - signalStrength + diffX]
+      const signalReachUp = [target, sensor.y + signalStrength - diffX]
+
+      intersectionRanges.push([signalReachDown, signalReachUp])
+    }
+
+  }
+
+  return intersectionRanges
+}
+
+///////////////////////////////////////////////////////////////
+
+const MAX = argument.includes('TEST') ? 20 : 4_000_000
+
+function isCovered(array) {
+
+  if (array[0] < 0 || array[0] > MAX || array[1] < 0 || array[1] > MAX)
+    return true
+
+  const Xintersections = GetIntersectionRangesFor(array[1], 'Y')
+  const Yintersections = GetIntersectionRangesFor(array[0], 'X')
+
+  for (const [start, end] of Xintersections) {
+
+    if (array[0] >= start[0] && array[0] <= end[0])
+      return true
+  }
+
+  for (const [start, end] of Yintersections) {
+
+    if (array[1] >= start[1] && array[1] <= end[1])
+      return true
+  }
+
+  return false
+}
+
+
+
+function bruteForceFindHiddenBeacon() {
+
+  for (const sensor of Object.values(SENSORS)) {
+
+    const beacon = BEACONS[sensor]
+    const signalReach = sensor.manhattenDist(beacon)
+
+    // Using array, instead of Vector class, to make things a bit faster.
+    const signalReachUp = [sensor.x, sensor.y + signalReach + 1]
+    const signalReachRight = [sensor.x + signalReach + 1, sensor.y]
+    const signalReachDown = [sensor.x, sensor.y - signalReach - 1]
+    const signalReachLeft = [sensor.x - signalReach - 1, sensor.y]
+
+    for (let i = signalReach + 1; i >= 0; i--) {
+
+      if (i % 10_000 == 0)
+        console.log('...Still looping some loops ', sensor, i)
+
+      if (!isCovered(signalReachUp)) return signalReachUp
+      if (!isCovered(signalReachRight)) return signalReachRight
+      if (!isCovered(signalReachDown)) return signalReachDown
+      if (!isCovered(signalReachLeft)) return signalReachLeft
+
+
+      signalReachUp[0]++
+      signalReachUp[1]--
+
+      signalReachRight[0]--
+      signalReachRight[1]--
+
+      signalReachDown[0]--
+      signalReachDown[1]++
+
+      signalReachLeft[0]++
+      signalReachLeft[1]++
+
+    }
+  }
+
+}
+
+
+console.log('\n This kinda takes a while:')
+console.group()
+const coord = bruteForceFindHiddenBeacon()
+console.log(`\nThe hidden Distressbeacon is at Location (${coord[0].toLocaleString()}, ${coord[1].toLocaleString()}) with a tuning frequency of ${(coord[0] * 4_000_000 + coord[1]).toLocaleString()}\n`)
+console.groupEnd()
